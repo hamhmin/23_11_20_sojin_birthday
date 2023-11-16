a7cb308c7895ce14944ebe5cb8f97061e65e76e6
hyungmin <hamhmin1031@gmail.com>
2023-11-16 18:56:16 +0900

    push

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    
+    var music = false;
+    $(".music_btn").on("click",function(){
+        if(!music){
+            music = true;
+        } else {
+            music = false;
+        }
+    });
 });
